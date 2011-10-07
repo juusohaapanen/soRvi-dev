7b58b28e8e19445888c5fbdcef3d9afd69f91cd7
lml <lml@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-07 23:50:53 +0000

    gadm.plot
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/sorvi@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/firstlib.R
+++ pkg/R/firstlib.R
@@ -8,6 +8,5 @@
 .onLoad <- function(lib, pkg)
 {
    #library.dynam('sorvi', pkg, lib) # for C libraries
-   cat('\nsorvi - avoimen datan tyokalupakki. Copyright (C) 2011 Leo Lahti ja Juuso Parkkinen. Tama on vapaa
-ohjelmisto, jota voi vapaasti kayttaa, muokata ja levittaa FreeBSD-lisenssilla.\n')
+   cat('\nsorvi - avoimen datan tyokalupakki. Copyright (C) 2011 Leo Lahti ja Juuso Parkkinen. Tama on vapaa ohjelmisto, jota voi vapaasti kayttaa, muokata ja levittaa FreeBSD-lisenssilla.\n')
 }
