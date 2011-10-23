60365d90efa04aafb34a658bd664e18069816b38
ouzo <ouzo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-23 18:16:08 +0000

    Juuso 23.10.2011
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/sorvi@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/googlemaps.R
@@ -0,0 +1,86 @@
+# Modified from https://github.com/hadley/ggplot2/wiki/Crime-in-Downtown-Houston,-Texas-:-Combining-ggplot2-and-Google-Maps
+get.staticmap.GoogleMaps <- function(center, zoom = 10, GRAYSCALE=FALSE, scale=2, maptype = 'map',
+                                     destfile = 'TemporaryMap.png', n_pix = 640, format="png32") {
+  library(RgoogleMaps)
+  library(ReadImages)
+  library(png)
+  library(maptools)
+  library(ggplot2)  
+
+  # Get map with given scale
+  if (scale==1) 
+    GetMap(center = center[c('lat','lon')], GRAYSCALE=GRAYSCALE, size = c(n_pix, n_pix), 
+           zoom = zoom, format = format, maptype = maptype, destfile=destfile)
+  else if (scale==2)
+    GetMap(center = center[c('lat','lon')], GRAYSCALE=GRAYSCALE, size = c(n_pix, n_pix),
+           zoom = zoom, format = format, maptype = paste(maptype, "&scale=2", sep=""), destfile=destfile)
+  else
+    stop("Invalid scale-value!")
+  
+  map <- readPNG(destfile)
+  n_pix <- n_pix*scale #Double number of pixels if scale==2
+  
+  # Deal with color
+  if(GRAYSCALE == FALSE) {
+    cat("Colours: yes\n")
+    map <- apply(map, 1:2, function(v) rgb(v[1], v[2], v[3]))     
+  } else {
+    cat("Colours: no\n")
+    nrow <- nrow(map)
+    ncol <- ncol(map)
+    map <- grey(rgb2grey(map, coefs = c(0, 1, 0)))
+    map <- matrix(map, nrow = nrow, ncol = ncol)
+  }
+
+  # Reshape map for plotting
+  m_map <- melt(map)
+  names(m_map) <- c('x','y','fill')
+  m_map <- within(m_map,{
+    x <- x - n_pix/2 - 1
+    y <- y - n_pix/2 - 1
+  })     
+  
+  mapInfo <- list(lat = center['lat'], lon = center['lon'], zoom = zoom, map)
+  XY_cent <- LatLon2XY.centered(mapInfo, center['lat'], center['lon'])
+  
+  # Geocode pixel references
+  s <- (-n_pix/2) : (n_pix/2 - 1)  
+  lat_wrapper <- function(x) XY2LatLon(mapInfo, -n_pix/2, x)[1]
+  lats <- apply(data.frame(s), 1, lat_wrapper)  
+  lon_wrapper <- function(y) XY2LatLon(mapInfo, y, -n_pix/2)[2]
+  lons <- apply(data.frame(s), 1, lon_wrapper)
+  
+  # Merge colors to latlons and return
+  df_xy   <- expand.grid(x = s, y = s)
+  df_ll   <- expand.grid(lat = rev(lats), lon = lons)
+  df_xyll <- data.frame(df_xy, df_ll)
+  df <- suppressMessages(join(df_xyll, m_map, type = 'right'))
+  df <- df[,c('lon','lat','fill')]
+  return(df)
+}
+
+# Modified from http://www.r-chart.com/2010/07/maps-geocoding-and-r-user-conference.html
+get.geocode.GoogleMaps <- function(str) {
+  library(XML)
+  u <- paste('http://maps.google.com/maps/api/geocode/xml?sensor=false&address=',str)
+  doc <- xmlTreeParse(u, useInternal=TRUE)
+  lat <- sapply(getNodeSet(doc, "/GeocodeResponse/result/geometry/location/lat"), function(el) xmlValue(el))
+  lon <- sapply(getNodeSet(doc, "/GeocodeResponse/result/geometry/location/lng"), function(el) xmlValue(el))
+  return(c(lat,lon))
+}
+
+# Get geocode using OpenStreetMap
+get.geocode.OpenStreetMap <- function(query) {
+  library(RCurl)
+  library(rjson)
+  u <- paste("http://nominatim.openstreetmap.org/search?q=",query,"&format=json", sep="")
+  val <- getURI(u)
+  res <- fromJSON(val)
+  if (length(res)>0)
+    return(as.numeric(c(res[[1]]$lat, res[[1]]$lon)))
+  else # Geocode not found
+    return(NULL)
+}
+
+
+
---
+++ pkg/R/preprocess.PKS.aluejakokartat.R
@@ -0,0 +1,35 @@
+library(rgdal)
+library(gpclib)
+library(gdata)
+
+preprocess.PKS.aluejakokartat <- function() {
+
+# Script for processing PKS aluejakokartat data
+# License: FreeBSD, http://en.wikipedia.org/wiki/BSD_licenses
+# Copyright 2011 Juuso Parkkinen, juuso.parkkinen@gmail.com. All rights reserved.
+
+# Need to install package rgdal
+# Mac users, see http://www.r-bloggers.com/installing-rgdal-on-mac-os-x-2/
+
+  gpclibPermit()
+
+pks.pienalue <- readOGR(dsn="data/PKS_Kartta_Rajat_KML2011/PKS_pienalue2.kml", layer="pks_pienalue")
+
+pks.pienalue@data$id <- rownames(pks.pienalue@data) # Add IDs
+pks.points <- fortify.SpatialPolygonsDataFrame(pks.pienalue, region="id") # Get point data
+pks.points$group <- sub(".1", "", pks.points$group) # Regex PIEN to joinable format
+
+pks.df <- merge(pks.points, pks.pienalue@data, by.x="group", by.y = "id") # Put everything together
+pks.df <- pks.df[order(pks.df$order),] # sort DF so that polygons come out in the right order
+
+# Fix encoding
+pks.df$Nimi <- factor(iconv(pks.df$Nimi, from="ISO-8859-1", to="UTF-8"))
+pks.df$NIMI_ISO <- factor(iconv(pks.df$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
+pks.df$Name <- factor(iconv(pks.df$Name, from="ISO-8859-1", to="UTF-8"))
+pks.pienalue@data$Nimi <- factor(iconv(pks.pienalue@data$Nimi, from="ISO-8859-1", to="UTF-8"))
+pks.pienalue@data$NIMI_ISO <- factor(iconv(pks.pienalue@data$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
+pks.pienalue@data$Name <- factor(iconv(pks.pienalue@data$Name, from="ISO-8859-1", to="UTF-8"))
+
+# Save data
+save(pks.pienalue, pks.df, file="data/PKS_aluejakokartat.rda")
+}
