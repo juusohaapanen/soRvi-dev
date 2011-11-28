926060b96e5ff6ef188cd175c7e905475ed66522
lml <lml@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-28 16:16:44 +0000

    asennusohjeita kohennettu
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/sorvi@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/examples/sorvi.installation.R
+++ www/examples/sorvi.installation.R
@@ -1,27 +1,38 @@
 # soRvi-pakettia varten suositeltujen pakettien asennusskripti
 
+# Kayttoohjeet: 
+# 1) Asenna uusin R-versio (r-project.org)
+# 2) Kaynnista R
+# 3) Aja tama skripti R:n komentorivilta kirjoittamalla
+#    source("http://sorvi.r-forge.r-project.org/examples/sorvi.installation.R")
+#    (pakettien asennus edellyttaa toimivaa verkkoyhteytta)
 
-if (!require(gdata)) {try(install.packages("gdata"))}
-if (!require(ggplot2)) {try(install.packages("ggplot2"))}
-if (!require(gpclib)) {try(install.packages("gpclib"))}
-if (!require(mapproj)) {try(install.packages("mapproj"))}
-if (!require(maps)) {try(install.packages("maps"))}
-if (!require(maptools)) {try(install.packages("maptools"))}
-if (!require(plyr)) {try(install.packages("plyr"))}
-if (!require(png)) {try(install.packages("png"))}
-if (!require(pxR)) {try(install.packages("pxR"))}
-if (!require(ReadImages)) {try(install.packages("ReadImages"))}
-if (!require(RgoogleMaps)) {try(install.packages("RgoogleMaps"))}
-if (!require(rjson)) {try(install.packages("rjson"))}
-if (!require(sp)) {try(install.packages("sp"))}
-if (!require(XML)) {try(install.packages("XML"))}
-if (!require(RCurl)) {try(install.packages("RCurl")})
-if (!require(rgdal)) {try(install.packages("rgdal"))}
-if (!require(rgeos)) {try(install.packages("rgeos"))}
-if (!require(rgl)) {try(install.packages("rgl"))}
-if (!require(rworldmap)) {try(install.packages("rworldmap"))}
-if (!require(gridExtra)) {try(install.packages("gridExtra"))}
-#if (!require()) {try(install.packages(""))}
+# Suppress error messages during installation
+silent <- TRUE
+
+# Install the packages
+if (!require(gdata)) {try(install.packages("gdata"), silent = silent)}
+if (!require(ggplot2)) {try(install.packages("ggplot2"), silent = silent)}
+if (!require(gpclib)) {try(install.packages("gpclib"), silent = silent)}
+if (!require(gridExtra)) {try(install.packages("gridExtra"), silent = silent)}
+if (!require(mapproj)) {try(install.packages("mapproj"), silent = silent)}
+if (!require(maps)) {try(install.packages("maps"), silent = silent)}
+if (!require(maptools)) {try(install.packages("maptools"), silent = silent)}
+if (!require(plyr)) {try(install.packages("plyr"), silent = silent)}
+if (!require(png)) {try(install.packages("png"), silent = silent)}
+if (!require(pxR)) {try(install.packages("pxR"), silent = silent)}
+if (!require(RCurl)) {try(install.packages("RCurl"), silent = silent)}
+if (!require(ReadImages)) {try(install.packages("ReadImages"), silent = silent)}
+if (!require(rgdal)) {try(install.packages("rgdal"), silent = silent)}
+if (!require(rgeos)) {try(install.packages("rgeos"), silent = silent)}
+if (!require(rgl)) {try(install.packages("rgl"), silent = silent)}
+if (!require(RgoogleMaps)) {try(install.packages("RgoogleMaps"), silent = silent)}
+if (!require(rjson)) {try(install.packages("rjson"), silent = silent)}
+if (!require(rworldmap)) {try(install.packages("rworldmap"), silent = silent)}
+if (!require(sp)) {try(install.packages("sp"), silent = silent)}
+if (!require(XML)) {try(install.packages("XML"), silent = silent)}
+#if (!require()) {try(install.packages(""), silent = silent)}
+
 
 install.packages("sorvi", repos="http://R-Forge.R-project.org", type = "source", dependencies = TRUE)
 
