3db4d897be3d2ab15bbbfd6de3479d4a395a053d
lml <lml@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-18 21:20:13 +0000

    roxygenization continues
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/sorvi@88 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/data.documentation.R
@@ -0,0 +1,40 @@
+#' MML data documentation 
+#'
+#' The MML data set contains preprocessed map data obtained from 
+#' Maanmittauslaitos (MML; Finnish geographical agency). 
+#' Copyright (C) MML 2011. 
+#' The licenses allow modification and redistribution of the data.
+#' 
+#' @name MML
+#' @docType data
+#' @description (C) MML 2011.
+#' @author Leo Lahti \email{sorvi-commits@lists.r-forge.r-project.org} 
+#' is responsible of data preprocessing.
+#' @references \url{http://www.maanmittauslaitos.fi/aineistot-palvelut/digitaaliset-tuotteet/ilmaiset-aineistot/hankinta lisenssiehtojen mukaisesti: http://www.maanmittauslaitos.fi/node/6417}
+#' @usage data(MML)
+#' @format list
+#' @keywords data
+NULL
+
+
+
+#' translations data documentation 
+#'
+#' This data set contains translations for common terms for which
+#' primary data is available in a non-Finnish language
+#' 
+#' @name translations
+#' @docType data
+#' @description Translations for common terms for which primary data is available in a non-Finnish language. 
+#' @details Contents: fi.en.maakunnat: Province names Finnish-English
+#' @author Leo Lahti \email{sorvi-commits@lists.r-forge.r-project.org} 
+#' @references See cite(sorvi)
+#' @usage data(translations)
+#' @format list
+#' @keywords data, misc
+NULL
+
+
+
+
+
