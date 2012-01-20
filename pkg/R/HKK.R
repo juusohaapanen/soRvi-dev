2297d6881fd63afd82292d5fbc372b224260d6d7
Joona Lehtomaki <joona.lehtomaki@gmail.com>
2012-01-20 20:30:20 +0200

    Downloading and aux data parsing done

--- pkg/R/HKK.R
+++ pkg/R/HKK.R
@@ -12,6 +12,34 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+.parse.df <- function(df) {
+  # Create an empty list to hold the expanded rows as elements
+  new.rows <- list()
+  # Loop over the rows in the data frame
+  for (i in 1:nrow(df)) {
+    # Get the current row (2 elements)
+    row <- df[i,]
+    # Split the 1st elemnt by white space -> ressult in character vector with 
+    # 3 or 4 elements (kuntaID, piiriID, piiri (A-part) [piiri (B-part)])
+    cell.raw <- unlist(strsplit(row[[1]], "\\s"))
+    # Created the expanded verision by joining elemnents 1 and 2 as they are
+    # and concatenating elements 3 and 4 (A- and B-part) together if piiri 
+    # constitutes of 2 parts
+    cell.expanded <- c(cell.raw[1:2], paste(cell.raw[3:length(cell.raw)], 
+                                            collapse=' '))
+    # Create a new 4 item row by joining the expanded cell with aanestysaluekoodi
+    # (also trim the leading white space)
+    new.rows[[i]] <- c(cell.expanded, trim(row[[2]]))
+  }
+  # Merge the list of vectors (new rows) into a matrix
+  new.df <- do.call("rbind", new.rows)
+  # Create the header
+  colnames(new.df) <- c("kuntaID", "piiriID", "piiri", "aanestysaluekoodi")
+  # Return a data frame
+  return(data.frame(new.df))
+}
+res <- .parse.df(df1)
+
 #' Retrieve HKK data 
 #'
 #' This script retrieves data from Helsinki Real Estate Department (Helsingin 
@@ -22,14 +50,19 @@
 #' The data copyright is on Helsingin kaupunkimittausosasto (C)  2011.
 #' 
 #' @param which.data  A string. Specify the name of the HKK data set to retrieve. Currently available options: Aluejakokartat;Aanestysjakoalue;Seutukartta Rakennustietoruudukko; SeutuRAMAVA; key.KATAKER.
+#' @param data.dir A string. Specify the path where to save the downloaded data. A new subdfolder "aanestysalueet" will be created.
 #'
 #' @author Joona Lehtomäki \email{sorvi-commits@@lists.r-forge.r-project.org}
 #' @export
-GetHKK <- function(which.data) {
+GetHKK <- function(which.data, data.dir) {
   # TODO: shold all the urls/paths be defined independently from the functions?
   data.url <- "http://kartta.hel.fi/avoindata/aineistot/"
 
   if (which.data == "Aanestysjakoalue") {
+    if (!require(XLConnect)) {
+      try(install.packages("XLConnect"))
+    }
+    
     # Remote zip that will be downloaded
     remote.zip <- "pk_seudun_aanestysalueet.zip"
     # Location and name of the zip file that will be saved on the local computer
@@ -39,13 +72,31 @@ GetHKK <- function(which.data) {
     message(paste("Dowloading HKK data from ", data.url, "in file", local.zip))
     download.file(data.url, destfile = local.zip)
     
+    # Unzip the downloaded zip file
+    data.dir <- file.path(data.dir, "aanestysalueet")
+    unzip(local.zip, exdir=data.dir)
+    # Extract the data from respective zips for each city
+    city.zips <- list.files(path=data.dir, pattern="*[.zip]$", full.names=TRUE)
+    for (city.zip in city.zips) {
+      unzip(city.zip, exdir=data.dir)
+    }
+    
     # Construct the name of the Excel file containing additional data on voting
     # districts
-    xls.file.name <- file.path(find.package("sorvi"), "extdata", 
-                               "Aanestyaluekoodit.xls")
+    xls.file.name <- file.path(data.dir, "Aanestyaluekoodit.xls")
     
-    xls.sheets <- list('Helsinki'=1, 'Espoo'=2, 'Kauniainen'=3, 'Vantaa'=4)
-    aux.dfs  <- lapply(xls.sheets, function(x) read.xls(xls.file.name, sheet = x))
+    xls.sheets <- list('Helsinki'=1, 'Espoo'=2, 'Vantaa'=3, 'Kauniainen'=4)
+    aux.dfs  <- lapply(xls.sheets, 
+                        function(x) readWorksheetFromFile(xls.file.name, 
+                                                          sheet=x,
+                                                          header = TRUE, 
+                                                          startRow = 2))
+    
+    # Each auxilary data frame has 2 columns: Aanestysalue and aanestysaluekoodi
+    # 1st cell of each row  contains 1) city ID, 2) district ID and 
+    # 3) disrict name
+    # Parse the 1st cell of each row into 3 individual elements
+    aux.dfs <- lapply(aux.dfs, function(x) .parse.df(x))
     
   } else if (which.data == "Aluejakokartat") {
     stop("Not implemented yet; Try GetHRIaluejakokartat instead")
