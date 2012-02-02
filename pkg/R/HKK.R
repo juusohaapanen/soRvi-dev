462c3108ef231fdb42f7c3fbbcd160af970c6698
Joona Lehtomaki <joona.lehtomaki@gmail.com>
2012-02-02 13:27:51 +0200

    Migrated MergeSpatial to dev-jlehtoma

--- pkg/R/HKK.R
+++ pkg/R/HKK.R
@@ -12,6 +12,29 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+
+.check.schema <- function(sp.object1, sp.object2) {
+  
+  # Check that both objects derive from Spatial
+  if (!is(sp.object1) || !is(sp.object2)) {
+    warning("Objects must derive from Spatial class")
+    return(FALSE)
+  }
+  
+  # First check that the attribute tables (data frames) have same number of cols
+  if (length(sp.object1@data) != sp.object2@data) {
+    warning("Spatial objects' data frames are different length")
+    return(FALSE)
+  } else {
+    # Check the attribute tables have the same colnames
+    if (names(sp.object1@data) == names(sp.object2@data)) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  }
+}
+
 .pad.zeros <- function(x, target.length=4) {
   x <- as.character(x)
   char.diff <- target.length - nchar(x)
@@ -71,7 +94,7 @@
 GetHKK <- function(which.data, data.dir) {
   # TODO: shold all the urls/paths be defined independently from the functions?
   data.url <- "http://kartta.hel.fi/avoindata/aineistot/"
-
+  
   if (which.data == "Aanestysaluejako") {
     if (!require(XLConnect)) {
       try(install.packages("XLConnect"))
@@ -101,10 +124,10 @@ GetHKK <- function(which.data, data.dir) {
     xls.sheets <- list('Helsinki'='Helsinki', 'Espoo'='Espoo', 
                        'Vantaa'='Vantaa', 'Kauniainen'='Kauniainen')
     aux.dfs  <- lapply(xls.sheets, 
-                        function(x) readWorksheetFromFile(xls.file.name, 
-                                                          sheet=x,
-                                                          header = TRUE, 
-                                                          startRow = 2))
+                       function(x) readWorksheetFromFile(xls.file.name, 
+                                                         sheet=x,
+                                                         header = TRUE, 
+                                                         startRow = 2))
     
     # Each auxilary data frame has 2 columns: Aanestysalue and aanestysaluekoodi
     # 1st cell of each row  contains 1) city ID, 2) district ID and 
@@ -117,12 +140,12 @@ GetHKK <- function(which.data, data.dir) {
     mapinfo.files["Helsinki"] <- list.files(path=data.dir, pattern="hki.*TAB$", 
                                             full.names=TRUE, ignore.case=TRUE)
     mapinfo.files["Espoo"] <- list.files(path=data.dir, pattern="espoo.*TAB$", 
-                                          full.names=TRUE, ignore.case=TRUE)
+                                         full.names=TRUE, ignore.case=TRUE)
     mapinfo.files["Kauniainen"] <- list.files(path=data.dir, 
                                               pattern="kauniainen.*TAB$", 
                                               full.names=TRUE, ignore.case=TRUE)
     mapinfo.files["Vantaa"] <- list.files(path=data.dir, pattern="vantaa.*TAB$", 
-                                            full.names=TRUE, ignore.case=TRUE)
+                                          full.names=TRUE, ignore.case=TRUE)
     # Read in the spatial data. A list is created with the names of the cities 
     # as keys and SpatialPolygonsDataFrames as values. CRS (KKJ2) is read 
     # directly from the data source. Each data source (MapInfo file) only has 1 
@@ -173,5 +196,84 @@ GetHKK <- function(which.data, data.dir) {
     stop("Not implemented yet")
   } else {
     stop(paste(which.data, "is not a valid data set descriptor"))
+  }
+}
+
+#' Merge a list of Spatial*DataFrame objects into one Spatial*DataFrame
+#'
+#' Several independent Spatatial*DataFrame objects held in a list can be merged
+#' into one object as long as all are of the same class. CRS 
+#' projections will be performed if target CRS is provided. If CRS is not 
+#' provided, the CRS of the first object will be used. If even one object is 
+#' missing a CRS, no projections are performed and there is no guarantee that
+#' merge will produce desired outcome.
+#'
+#' All schemas must match, a schema is checked on the basis of column names. An
+#' optional FID string can be give to name for a field (column) in table schema
+#' that will be used as FIDs. It's up to the user to check that FID values are
+#' unique.
+#'
+#' @param sp.list  A list of Spatial*DataFrame objects to be merged
+#' @param CRS A proj4string definign target CRS for the target Spatial*DataFrame object
+#' @param FID A string that names the column used as FID
+#'
+#' @return a list of Shape objects (from SpatialPolygonsDataFrame class)
+#' @export
+#' @seealso spChFIDs, spRbind
+#' @references
+#' See citation("sorvi") 
+#' @author Joona Lehtomaki \email{sorvi-commits@@lists.r-forge.r-project.org}
+#' @examples # sp <- GetHKK("Aanestysaluejako", data.dir="C:/data")
+
+MergeSpatial <- function(sp.list, CRS=NA, FID=NA) {
+  
+  allowed.classes <- c("SpatialPolygonsDataFrame", "SpatialPointsDataFrame",
+                       "SpatialLinesDataFrame")
+  
+  # Test that all the objects in the list are of the same class
+  # FIXME: what's the smartest way of testing whether an object is of a 
+  # particular class?
+  
+  # Get the class of the first sp object in the list
+  class.type <- class(sp.list[[1]])[[1]]
+  
+  # Only particular Spatial*DataFrame classes are allowed, here we check that the
+  # class of the 1st object is allowed
+  if (!class.type %in% allowed.classes) {
+    stop(paste("All objects must be instances of one of the following classes: ",
+               paste(allowed.classes, collapse=", ")))
+  }
+  
+  # Check that all objects have the same class
+  if(all(sapply(sp.cities, function(x) is(x, class.type)))) {
+    
+    # Check that all objects have the same schema
+    ref.schema <- sp.list[[1]]
+    
+    # Compare the schema of all the other objects
+    # FIXME: now the first object is compared against itself, this is a bit 
+    # stupid
+    if(all(sapply(sp.list, function(x) .check.schema(ref.schema, x))) {
+      if (is.na(FID)) {
+        # No FID field is provided, so create FID fields for all attribute
+        # tables (data frames). FID values must be unique over all tables.
+        row.fids <- 1:sum(sapply(sp.list, function(x) nrow(x)))
+        for (sp.object in sp.list) {
+          rows <- nrow(sp.object)
+          FID <- row.fids[1:rows]
+          sp.object@data <- spChFIDs(sp.object@data, FID)
+          # Remove the used FIDs
+          row.fids <- row.fids[rows:length(row.fids)]
+        }
+        return(do.call("spRbind", sp.list))
+        
+      } else {
+        
+      }
+    }
+       
+  } else {
+    stop(paste("All objects must be instances of one of the following class: ",
+               class.type))
   }
 }
